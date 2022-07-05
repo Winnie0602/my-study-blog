105468959a58eeb63204cf597c4dc4f0bbec6d63
張文馨 <winniechang@zhangwenxindeMacBook-Air.local>
2022-07-05 22:35:16 +0800

    second commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/my-study-blog/' // test20200915 為 repo 名稱
+    : '/'
+}
